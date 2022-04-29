595b11463aa449d9b85e2a6890a7fff354c101da
Valentin Mignon <valentin54112@gmail.com>
2022-04-29 14:20:25 +0200

    maj

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CV Valentin Mignon</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 </head>
 
 <body>
@@ -31,8 +29,7 @@
     <nav id="nav" class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Menu</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
@@ -64,8 +61,7 @@
                 </div>
                 <hr>
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 10%"
-                        aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <hr>
             </div>
@@ -132,8 +128,7 @@
                 </div>
                 <hr>
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 25%"
-                        aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <hr>
             </div>
@@ -175,8 +170,7 @@
                 </div>
                 <hr>
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 50%"
-                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <hr>
             </div>
@@ -243,8 +237,7 @@
                     </div>
                 </div>
                 <div class="card" style="width: 18rem;">
-                    <img src="./img/cora.png" class="card-img-top" alt="logo signature"
-                        style="background-color: white;">
+                    <img src="./img/cora.png" class="card-img-top" alt="logo signature" style="background-color: white;">
                     <div class="card-body">
                         <h5 class="card-title">Cora | Employé de rayon (frais)</h5>
                         <ul class="list-group list-group-flush">
@@ -278,45 +271,44 @@
                 </div>
                 <hr>
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 75%"
-                        aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <hr>
             </div>
             <div class="noir" id="compet">
                 <div class="one">
-                <div>
-                    <img src="./img/html.png" alt="">
-                </div>
-                <div>
-                    <img src="./img/javascript.png" alt="">
-                </div>
-                <div>
-                    <img src="./img/bootstrap.png" alt="">
-                </div>
-                <div>
-                    <img src="./img/mysql.png" alt="">
-                </div>
-                <div>
-                    <img src="./img/php.png" alt="">
-                </div>
+                    <div>
+                        <img src="./img/html.png" alt="">
+                    </div>
+                    <div>
+                        <img src="./img/javascript.png" alt="">
+                    </div>
+                    <div>
+                        <img src="./img/bootstrap.png" alt="">
+                    </div>
+                    <div>
+                        <img src="./img/mysql.png" alt="">
+                    </div>
+                    <div>
+                        <img src="./img/php.png" alt="">
+                    </div>
                 </div>
                 <div class="two">
-                <div>
-                    <img id="logocss" class="centre" src="./img/css.png" alt="">
-                </div>
-                <div>
-                    <img class="centre" src="./img/github.png" alt="">
-                </div>
-                <div>
-                    <img class="centre" src="./img/figma.jpg" alt="">
-                </div>
-                <div>
-                    <img class="centre" src="./img/phpmyadmin.png" alt="">
-                </div>
-                <div>
-                    <img id="logooffice" class="centre" src="./img/office4.png" alt="">
-                </div>
+                    <div>
+                        <img id="logocss" class="centre" src="./img/css.png" alt="">
+                    </div>
+                    <div>
+                        <img class="centre" src="./img/github.png" alt="">
+                    </div>
+                    <div>
+                        <img class="centre" src="./img/figma.jpg" alt="">
+                    </div>
+                    <div>
+                        <img class="centre" src="./img/phpmyadmin.png" alt="">
+                    </div>
+                    <div>
+                        <img id="logooffice" class="centre" src="./img/office4.png" alt="">
+                    </div>
                 </div>
             </div>
         </section>
@@ -328,8 +320,7 @@
                 </div>
                 <hr>
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 100%"
-                        aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 87%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <hr>
             </div>
@@ -354,14 +345,35 @@
         </section>
         <!-- </section> -->
     </section>
+    <section class="bloc" id="form">
+        <div class="gris">
+            <div>
+                <h2>Me laisser un petit message </h2>
+            </div>
+            <hr>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 100%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <hr>
+        </div>
+        <div class="noir">
+            <form action="action.php" method="post">
+                <p>Votre nom : <input type="text" name="nom" /></p>
+                <p>Votre prénom : <input type="text" name="prenom" /></p>
+                <p>Je vous écoute :<input type="text-area" name="avis" /></p>
+                <p>Votre email : <input type="mail" name="email"></p>
+                <p><input type="submit" value="Envoyez !"></p>
+            </form>
+        </div>
+
+    </section>
     <footer>
         <div id="reseaux">
             <div class="logs">
                 <a href="https://github.com/ValDev99"><img src="./img/github.png" alt="logo github"></a>
             </div>
             <div class="logs">
-                <a href="https://www.linkedin.com/in/valentin-mignon-2a4345204/"><img src="./img/linkedin.png"
-                        alt="logo linkedin"></a>
+                <a href="https://www.linkedin.com/in/valentin-mignon-2a4345204/"><img src="./img/linkedin.png" alt="logo linkedin"></a>
             </div>
             <div class="logs">
                 <a href=""><img src="./img/logocv.png" alt="cv"></a>
